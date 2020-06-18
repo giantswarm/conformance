0654b2eb445082107f8fd474f388ad97d4bca8f8
Nick Jüttner <nick@giantswarm.io>
2020-06-18 16:35:04 +0200

    init

--- e2e.go
+++ e2e.go
@@ -6,6 +6,7 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/giantswarm/conformance/framework"
+
 	// ensure that cloud providers are loaded
 	_ "github.com/giantswarm/conformance/framework/providers/aws"
 	_ "github.com/giantswarm/conformance/framework/providers/azure"
@@ -20,25 +21,3 @@ func RunE2ETests(t *testing.T) {
 	p.Create()
 	logrus.Println("Blob")
 }
-
-//func createClient() kubernetes.Interface {
-//	kubeconfig := os.Getenv("KUBECONFIG")
-//
-//	var cfg *rest.Config
-//	var err error
-//	if kubeconfig != "" {
-//		cfg, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
-//	} else {
-//		cfg, err = rest.InClusterConfig()
-//	}
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	kubeClient, err := kubernetes.NewForConfig(cfg)
-//	if err != nil {
-//		panic(err)
-//	}
-//	return kubeClient
-//}
-//
